9661ac8172c2a19aaecee5f9bdc7458b59541189
Code.X <1253478653@qq.com>
2020-02-01 20:15:05 +0800

    fix: 🐛 button样式修改

--- examples/src/pages/Button/index.tsx
+++ examples/src/pages/Button/index.tsx
@@ -1,31 +1,26 @@
 import React from 'react';
 import { Button } from '../../../../lib';
+import CodeShow from '../../components/CodeShow';
 import './index.scss';
 
 const ExampleButton = () => {
 	return <div className='example-button'>
-		<div className='example-button_container'>
-			<h1>基本用法</h1>
-			<div className='example-button_demo-1'>
-				<Button className='marginRight10'>Default</Button>
-				<Button type='primary' className='marginRight10'>Primary</Button>
-				<Button type='dashed' className='marginRight10'>Dashed</Button>
-				<Button type='danger' className='marginRight10'>Danger</Button>
-				<Button type='link' className='marginRight10'>Link</Button>
-				<Button type='link' className='marginRight10'>
-					<span>Hello World</span>
-				</Button>
-			</div>
-		</div>
+		<CodeShow title='基本用法'>
+			<Button className='marginRight10'>Default</Button>
+			<Button type='primary' className='marginRight10'>Primary</Button>
+			<Button type='dashed' className='marginRight10'>Dashed</Button>
+			<Button type='danger' className='marginRight10'>Danger</Button>
+			<Button type='link' className='marginRight10'>Link</Button>
+			<Button type='link' className='marginRight10'>
+				<span>Hello World</span>
+			</Button>
+		</CodeShow>
 
-		<div className='example-button_container'>
-			<h1>Size 大小</h1>
-			<div className='example-button_demo-1'>
-				<Button size='small' className='marginRight10'>Default</Button>
-				<Button size='default' type='primary' className='marginRight10'>Primary</Button>
-				<Button size='large' className='marginRight10'>Dashed</Button>
-			</div>
-		</div>
+		<CodeShow title='Size大小' style={{marginTop: 30}}>
+			<Button size='small' className='marginRight10'>Default</Button>
+			<Button size='default' type='primary' className='marginRight10'>Primary</Button>
+			<Button size='large' className='marginRight10'>Dashed</Button>
+		</CodeShow>
 	</div>;
 };
 
--- examples/src/pages/Icon/index.tsx
+++ examples/src/pages/Icon/index.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import { Icon } from '../../../../lib';
+import CodeShow from '../../components/CodeShow';
 import './index.scss';
 
 const ExampleIcon = () => {
 	return <div className='example-icon'>
-		<div className='example-icon_container'>
-			<h1>基本用法</h1>
-			<div className='example-icon_demo-1'>
-				<Icon type='close' className='marginRight10' />
-				<Icon type='all' className='marginRight10' />
-				<Icon type='filter' className='marginRight10' />
-				<Icon type='copy' className='marginRight10' fill='red' />
-				<Icon type='good' className='marginRight10' fill='green' />
-				<Icon type='auto' spin={true} className='marginRight10' />
-				<Icon type='add' spin={true} className='marginRight10' />
-				<Icon type='loading' spin={true} fill='#1890ff' interval={1200} className='marginRight10' />
-				<Icon type='atm' interval={500} className='marginRight10' />
-			</div>
-		</div>
+		<CodeShow title='基本用法'>
+			<Icon type='close' className='marginRight10' />
+			<Icon type='all' className='marginRight10' />
+			<Icon type='filter' className='marginRight10' />
+			<Icon type='copy' className='marginRight10' fill='red' />
+			<Icon type='good' className='marginRight10' fill='green' />
+			<Icon type='auto' spin={true} className='marginRight10' />
+			<Icon type='add' spin={true} className='marginRight10' />
+			<Icon type='loading' spin={true} fill='#1890ff' interval={1200} className='marginRight10' />
+			<Icon type='atm' interval={500} className='marginRight10' />
+		</CodeShow>
 	</div>;
 };
 
