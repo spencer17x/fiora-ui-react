1208b325371f1a19f75e804a96d2074303565891
Code.X <1253478653@qq.com>
2020-03-11 22:55:55 +0800

    feat: 🎸 grid组件

--- examples/src/pages/Grid/index.tsx
+++ examples/src/pages/Grid/index.tsx
@@ -1,29 +1,74 @@
 import React from "react";
 import CodeShow from "../../components/CodeCard";
 import { Row, Col } from "../../../../lib";
+import "./index.scss";
 
 export default function ExampleGrid() {
   return (
     <div className="example-grid">
-      <CodeShow title="基本用法">
+      <CodeShow
+        title="基本用法"
+        code={`
+          <>
+            <Row>
+              <Col className='example-grid-col light' span={24}>col</Col>
+            </Row>
+            <Row>
+              <Col className='example-grid-col light' span={12}>col-12</Col>
+              <Col className='example-grid-col dark' span={12}>col-12</Col>
+            </Row>
+            <Row>
+              <Col className='example-grid-col light' span={8}>col-8</Col>
+              <Col className='example-grid-col dark' span={8}>col-8</Col>
+              <Col className='example-grid-col light' span={8}>col-8</Col>
+            </Row>
+            <Row>
+              <Col className='example-grid-col light' span={6}>col-6</Col>
+              <Col className='example-grid-col dark' span={6}>col-6</Col>
+              <Col className='example-grid-col light' span={6}>col-6</Col>
+              <Col className='example-grid-col dark' span={6}>col-6</Col>
+            </Row>
+          </>
+        `}
+      >
         <>
           <Row>
-            <Col span={24}>col</Col>
+            <Col className="example-grid-col light" span={24}>
+              col
+            </Col>
           </Row>
           <Row>
-            <Col span={12}>col-12</Col>
-            <Col span={12}>col-12</Col>
+            <Col className="example-grid-col light" span={12}>
+              col-12
+            </Col>
+            <Col className="example-grid-col dark" span={12}>
+              col-12
+            </Col>
           </Row>
           <Row>
-            <Col span={8}>col-8</Col>
-            <Col span={8}>col-8</Col>
-            <Col span={8}>col-8</Col>
+            <Col className="example-grid-col light" span={8}>
+              col-8
+            </Col>
+            <Col className="example-grid-col dark" span={8}>
+              col-8
+            </Col>
+            <Col className="example-grid-col light" span={8}>
+              col-8
+            </Col>
           </Row>
           <Row>
-            <Col span={6}>col-6</Col>
-            <Col span={6}>col-6</Col>
-            <Col span={6}>col-6</Col>
-            <Col span={6}>col-6</Col>
+            <Col className="example-grid-col light" span={6}>
+              col-6
+            </Col>
+            <Col className="example-grid-col dark" span={6}>
+              col-6
+            </Col>
+            <Col className="example-grid-col light" span={6}>
+              col-6
+            </Col>
+            <Col className="example-grid-col dark" span={6}>
+              col-6
+            </Col>
           </Row>
         </>
       </CodeShow>
--- lib/components/grid/col.tsx
+++ lib/components/grid/col.tsx
@@ -1,10 +1,35 @@
 import React from "react";
 import classNames from "classnames";
+import PropTypes from "prop-types";
 import "./col.scss";
 
-interface ColProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-export default function Col(props: ColProps) {
-  const { children, className } = props;
-  return <div className={classNames("f-col", className)}>{children}</div>;
+interface ColProps extends React.HTMLAttributes<HTMLDivElement> {
+  span?: number;
 }
+
+const Col = (props: ColProps) => {
+  const { children, className, span } = props;
+  return (
+    <div
+      className={classNames(
+        "f-col",
+        {
+          [`f-col-${span}`]: span
+        },
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+Col.propTypes = {
+  span: PropTypes.number
+};
+
+Col.defaultProps = {
+  span: 24
+};
+
+export default Col;
