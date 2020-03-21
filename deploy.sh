9297505106aefcf5d85693ec01b992079748cee1
xuzpeng <xuzpeng@users.noreply.github.com>
2020-03-21 12:40:51 +0000

    deploy: f4d5ce0dda6af4a7d31e5da67a220f4de1c5cbba

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-npm version $1 && \
-    git pull && \
-    git push
--- node-tools/getWebpackEntries.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const fs = require('fs');
-
-module.exports = function getWebpackEntries(dir) {
-	return fs.readdirSync(path.resolve(dir));
-};
