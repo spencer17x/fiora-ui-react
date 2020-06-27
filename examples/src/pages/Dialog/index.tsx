e793c007d42c4184928a20cd6eb0fa3ee3197568
Spencer17x <1253478653@qq.com>
2020-06-27 17:43:06 +0800

    fix: 🐛 修改example、codemirror层级设为0

--- examples/src/pages/Dialog/index.tsx
+++ examples/src/pages/Dialog/index.tsx
@@ -24,7 +24,26 @@ const DemoDialog = () => {
 		}).then(value => console.log(value));
 	};
 	return <>
-		<CodeShow title='基本用法' style={{ marginBottom: '20px' }}>
+		<CodeShow
+			title='基本用法'
+			code={`
+const [v, setV] = useState(false);
+const onCloseV = () => setV(false);
+<Button onClick={() => setV(true)}>打开dialog</Button>
+<Dialog
+	title='Dialog 对话框'
+	visible={v}
+	onClose={onCloseV}
+	buttons={[
+		<Button onClick={onCloseV}>取消</Button>,
+		<Button type='primary' onClick={onCloseV}>确认</Button>,
+	]}
+>
+	<span>这是一段信息</span>
+</Dialog>
+			`}
+			style={{ marginBottom: '20px' }}
+		>
 			<Button onClick={() => setV(true)}>打开dialog</Button>
 			<Dialog
 				title='Dialog 对话框'
@@ -38,7 +57,30 @@ const DemoDialog = () => {
 				<span>这是一段信息</span>
 			</Dialog>
 		</CodeShow>
-		<CodeShow title='编码式弹窗'>
+		<CodeShow
+			title='编码式弹窗' code={`
+const onDialogAlert = () => {
+	Dialog.alert({
+		title: '标题',
+		content: '内容内容内容内容'
+	}).then(action => console.log(action));
+};
+const onDialogConfirm = () => {
+	Dialog.confirm({
+		title: '标题',
+		content: '内容内容内容内容'
+	}).then(action => console.log(action));
+};
+const onDialogPrompt = () => {
+	Dialog.prompt({
+		title: '标题',
+		content: '内容内容内容内容'
+	}).then(value => console.log(value));
+};
+<Button onClick={onDialogAlert}>Dialog Alert</Button>
+<Button onClick={onDialogConfirm}>Dialog Confirm</Button>
+<Button onClick={onDialogPrompt}>Dialog Prompt</Button>
+		`}>
 			<Button onClick={onDialogAlert}>Dialog Alert</Button>
 			<Button onClick={onDialogConfirm}>Dialog Confirm</Button>
 			<Button onClick={onDialogPrompt}>Dialog Prompt</Button>
--- examples/src/pages/Message/index.tsx
+++ examples/src/pages/Message/index.tsx
@@ -21,6 +21,14 @@ const DemoMessage = () => {
 	return <div className='exp-message'>
 		<CodeShow
 			title='基本用法'
+			code={`
+const info = () => {
+  message.info('This is a normal message');
+};
+<Button type="primary" onClick={info}>
+	Display normal message
+</Button>
+			`}
 		>
 			<Button type="primary" onClick={info}>
 				Display normal message
@@ -30,6 +38,22 @@ const DemoMessage = () => {
 		<CodeShow
 			style={{ marginTop: 30 }}
 			title='其他类型用法'
+			code={`
+const success = () => {
+	message.success('This is a success message');
+};
+
+const error = () => {
+	message.error('This is an error message');
+};
+
+const warning = () => {
+	message.warning('This is a warning message');
+};
+<Button onClick={success}>Success</Button>
+<Button onClick={error}>Error</Button>
+<Button onClick={warning}>Warning</Button>
+			`}
 		>
 			<Button onClick={success}>Success</Button>
 			<Button onClick={error}>Error</Button>
@@ -39,6 +63,11 @@ const DemoMessage = () => {
 		<CodeShow
 			style={{ marginTop: 30 }}
 			title='修改延时'
+			code={`
+<Button onClick={() => {
+	message.info('info 1 s', 1);
+}}>info 1 s</Button>
+			`}
 		>
 			<Button onClick={() => {
 				message.info('info 1 s', 1);
@@ -48,6 +77,22 @@ const DemoMessage = () => {
 		<CodeShow
 			style={{ marginTop: 30 }}
 			title='message 动画结束时回调'
+			code={`
+<Button
+	onClick={() => {
+		message.info('message 动画结束时回调1', 1, () => {
+			console.log('message 动画结束时回调1');
+		});
+	}}
+>message 动画结束时回调1</Button>
+<Button
+	onClick={() => {
+		message.info('message 动画结束时回调2', () => {
+			console.log('message 动画结束时回调2');
+		});
+	}}
+>message 动画结束时回调2</Button>
+			`}
 		>
 			<Button
 				onClick={() => {
