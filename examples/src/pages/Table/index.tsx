59c46b0a629d3400778b259d5b72fbf9c76a020d
Spencer17x <1253478653@qq.com>
2020-07-10 00:12:57 +0800

    feat: 🎸 table组件

--- examples/src/pages/Table/index.tsx
+++ examples/src/pages/Table/index.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import CodeShow from '../../components/CodeCard';
-import { Button, Table } from '../../../../lib';
+import { Button, Table, TableProps } from '../../../../lib';
 
 const DemoTable = () => {
-  const [columns] = useState([{
+  const [columns] = useState<TableProps['columns']>([{
     title: '姓名',
     dataIndex: 'name',
     key: 'name'
@@ -14,53 +14,197 @@ const DemoTable = () => {
   }, {
     title: '住址',
     dataIndex: 'address',
-    key: 'address'
+    key: 'address',
+  }]);
+
+  const [columns2] = useState<TableProps['columns']>([{
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name'
   }, {
-    title: '标签',
-    dataIndex: 'tags',
-    key: 'tags',
-    render: (tags: string[]) => {
-      return <>
-        {
-          tags.map((tag, index) => {
-            return <span key={index} style={{ marginRight: 10 }}>{tag}</span>;
-          })
-        }
-      </>;
-    }
+    title: '年龄',
+    dataIndex: 'age',
+    key: 'age'
+  }, {
+    title: '住址',
+    dataIndex: 'address',
+    key: 'address',
   }, {
     title: '操作',
-    key: 'action',
     dataIndex: 'action',
-    render: () => {
-      return <Button type='primary'>Giao</Button>;
+    key: 'action',
+    render(v, row) {
+      return <Button type='primary' onClick={() => console.log(row)}>
+        Giao
+      </Button>;
     }
   }]);
+
+  const [columns3] = useState<TableProps['columns']>([{
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name',
+    width: 300,
+    fixed: 'left'
+  }, {
+    title: '年龄',
+    dataIndex: 'age',
+    key: 'age',
+    width: 500
+  }, {
+    title: '住址',
+    dataIndex: 'address',
+    key: 'address',
+    width: 300,
+    fixed: 'right'
+  }]);
+
   const [data] = useState([{
     key: '1',
     name: 'John Brown',
     age: 32,
     address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer']
   }, {
     key: '2',
     name: 'Jim Green',
     age: 42,
     address: 'London No. 1 Lake Park',
-    tags: ['loser']
   }, {
     key: '3',
     name: 'Joe Black',
     age: 32,
     address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher']
   }]);
   return <div>
     <CodeShow
+      style={{ maxWidth: 960, marginBottom: 20 }}
       title='基本用法'
+      code={`
+const [columns] = useState<TableProps['columns']>([{
+  title: '姓名',
+  dataIndex: 'name',
+  key: 'name'
+}, {
+  title: '年龄',
+  dataIndex: 'age',
+  key: 'age'
+}, {
+  title: '住址',
+  dataIndex: 'address',
+  key: 'address',
+}]);
+const [data] = useState([{
+  key: '1',
+  name: 'John Brown',
+  age: 32,
+  address: 'New York No. 1 Lake Park',
+}, {
+  key: '2',
+  name: 'Jim Green',
+  age: 42,
+  address: 'London No. 1 Lake Park',
+}, {
+  key: '3',
+  name: 'Joe Black',
+  age: 32,
+  address: 'Sidney No. 1 Lake Park',
+}]);
+<Table
+  columns={columns}
+  data={data}
+/>
+      `}
     >
       <Table
         columns={columns}
+        data={data}
+      />
+    </CodeShow>
+
+    <CodeShow
+      style={{ maxWidth: 960, marginBottom: 20 }}
+      title='单元格渲染'
+      code={`
+const [columns2] = useState<TableProps['columns']>([{
+  title: '姓名',
+  dataIndex: 'name',
+  key: 'name'
+}, {
+  title: '年龄',
+  dataIndex: 'age',
+  key: 'age'
+}, {
+  title: '住址',
+  dataIndex: 'address',
+  key: 'address',
+}, {
+  title: '操作',
+  dataIndex: 'action',
+  key: 'action',
+  render(v, row) {
+    return <Button type='primary' onClick={() => console.log(row)}>
+      Giao
+    </Button>;
+  }
+}]);
+const [data] = useState([{
+  key: '1',
+  name: 'John Brown',
+  age: 32,
+  address: 'New York No. 1 Lake Park',
+}, {
+  key: '2',
+  name: 'Jim Green',
+  age: 42,
+  address: 'London No. 1 Lake Park',
+}, {
+  key: '3',
+  name: 'Joe Black',
+  age: 32,
+  address: 'Sidney No. 1 Lake Park',
+}]);
+<Table
+  columns={columns}
+  data={data2}
+/>
+      `}
+    >
+      <Table
+        columns={columns2}
+        data={data}
+      />
+    </CodeShow>
+
+    <CodeShow
+      style={{ maxWidth: 960 }}
+      title='固定列'
+      code={`
+const [columns3] = useState<TableProps['columns']>([{
+  title: '姓名',
+  dataIndex: 'name',
+  key: 'name',
+  width: 300,
+  fixed: 'left'
+}, {
+  title: '年龄',
+  dataIndex: 'age',
+  key: 'age',
+  width: 500
+}, {
+  title: '住址',
+  dataIndex: 'address',
+  key: 'address',
+  width: 300,
+  fixed: 'right'
+}]);
+<Table
+  columns={columns3}
+  data={data}
+/>
+      `}
+    >
+      <Table
+        columns={columns3}
         data={data}
       />
     </CodeShow>
--- lib/components/table/index.tsx
+++ lib/components/table/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import './index.scss';
 
 export type TableDataItem = {
@@ -10,7 +11,9 @@ export interface TableColumn {
   key: string;
   title?: string;
   dataIndex?: string;
-  render?: (v: any) => any;
+  width?: number;
+  render?: (v: any, row: TableDataItem) => any;
+  fixed?: 'left' | 'right'
 }
 
 export interface TableProps {
@@ -20,37 +23,56 @@ export interface TableProps {
 
 const Table: React.FC<TableProps> = props => {
   const { data, columns } = props;
-  return <table className='f-table'>
-    <thead className='f-table-thead'>
-      <tr className='f-table-tr'>
+  return <div className='f-table-container'>
+    <table className='f-table'>
+      <colgroup>
         {
-          columns.map(col => {
-            return <th className='f-table-th' key={col.key}>{col.title}</th>;
+          columns.map((col, index) => {
+            const { width = 'auto' } = col;
+            return <col key={index} style={{ width, minWidth: width }} />;
           })
         }
-      </tr>
-    </thead>
-    <tbody>
-      {
-        data.map(dataItem => {
-          return <tr className='f-table-tr' key={dataItem.key}>
-            {
-              columns.map(col => {
-                const { key, dataIndex = '' } = col;
-                return <td className='f-table-td' key={key}>
-                  {
-                    col.render === undefined ?
-                      dataItem[dataIndex] :
-                      col.render(dataItem[dataIndex])
-                  }
-                </td>;
-              })
-            }
-          </tr>;
-        })
-      }
-    </tbody>
-  </table>;
+      </colgroup>
+      <thead className='f-table-thead'>
+        <tr className='f-table-tr'>
+          {
+            columns.map(col => {
+              const { fixed, title, key } = col;
+              return <th
+                className={classNames('f-table-th', {
+                  [`fixed-${fixed}`]: fixed
+                })}
+                key={key}
+              >{title}</th>;
+            })
+          }
+        </tr>
+      </thead>
+      <tbody>
+        {
+          data.map(dataItem => {
+            return <tr className='f-table-tr' key={dataItem.key}>
+              {
+                columns.map(col => {
+                  const { key, dataIndex = '', fixed } = col;
+                  const renderValue = dataItem[dataIndex];
+                  return <td className={classNames('f-table-td', {
+                    [`fixed-${col.fixed}`]: fixed
+                  })} key={key}>
+                    {
+                      col.render === undefined ?
+                        renderValue :
+                        col.render(renderValue, dataItem)
+                    }
+                  </td>;
+                })
+              }
+            </tr>;
+          })
+        }
+      </tbody>
+    </table>
+  </div>;
 };
 
 Table.propTypes = {
@@ -62,7 +84,9 @@ Table.propTypes = {
       title: PropTypes.string.isRequired,
       key: PropTypes.string.isRequired,
       dataIndex: PropTypes.string.isRequired,
-      render: PropTypes.func.isRequired
+      render: PropTypes.func.isRequired,
+      width: PropTypes.number.isRequired,
+      fixed: PropTypes.oneOf<'left' | 'right'>(['left', 'right']).isRequired
     }).isRequired
   ).isRequired
 };
