2df8e7dba4f08c60d20c1940fe62f24f3353906a
Spencer17x <1253478653@qq.com>
2020-07-09 15:19:56 +0800

    feat: 🎸 form添加代码展示

--- examples/src/pages/Form/index.tsx
+++ examples/src/pages/Form/index.tsx
@@ -31,6 +31,40 @@ const DemoForm = () => {
     <CodeShow
       title='基础用法'
       style={{ marginBottom: 20 }}
+      code={`
+const [formData, setFormData] = useState<FormProps['data']>({
+  username: '',
+  password: '',
+  text: ''
+});
+
+const [fields] = useState<FormProps['fields']>([
+  { name: 'username', type: 'text', label: 'Username' },
+  { name: 'password', type: 'password', label: 'Password' },
+  { name: 'text', type: 'text', label: 'Text' }
+]);
+const [rules] = useState<FormProps['rules']>([
+  { name: 'username', required: true, message: '必填' },
+  { name: 'username', minlength: 5, message: '不得小于5' },
+  { name: 'username', maxlength: 15, message: '不得大于15' },
+  { name: 'password', required: true, message: '必填' },
+  { name: 'password', minlength: 5, message: '不得小于5' },
+  { name: 'password', maxlength: 15, message: '不得大于15' }
+]);
+
+<Form
+  data={formData}
+  fields={fields}
+  rules={rules}
+  onChange={data => setFormData(data)}
+  errors={errors}
+  buttons={
+    [
+      <Button type='primary' onClick={onSubmit}>提交</Button>
+    ]
+  }
+></Form>
+      `}
     >
       <Form
         data={formData}
@@ -48,6 +82,39 @@ const DemoForm = () => {
 
     <CodeShow
       title='layout 布局'
+      code={`
+<h3>vertical</h3>
+<Form
+  layout='vertical'
+  data={formData}
+  fields={fields}
+  rules={rules}
+  onChange={data => setFormData(data)}
+  errors={errors}
+  style={{ marginBottom: 20 }}
+  buttons={
+    [
+      <Button type='primary' onClick={onSubmit}>提交</Button>
+    ]
+  }
+></Form>
+
+<h3>inline</h3>
+<Form
+  layout='inline'
+  data={formData}
+  fields={fields}
+  rules={rules}
+  onChange={data => setFormData(data)}
+  errors={errors}
+  buttons={
+    [
+      <Button type='primary' onClick={onSubmit}>提交</Button>
+    ]
+  }
+>
+</Form>
+      `}
     >
       <h3>vertical</h3>
       <Form
