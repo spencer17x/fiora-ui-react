07fba97378fa56d32dabfe67fc841d26ba331657
Spencer17x <1253478653@qq.com>
2020-07-16 16:41:26 +0800

    feat: 🎸 修改eslint配置改为airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,6 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
-	extends: [
-		'plugin:@typescript-eslint/recommended',
-		//		'plugin:react/recommended'
-	],
-	plugins: ['@typescript-eslint'],
-	env: {
-		browser: true,
-		node: true
-	},
-	settings: {
-		'react': {
-			'pragma': 'React',
-			'version': 'detect'
-		}
-	},
+	extends: ['airbnb-typescript/base'],
 	parserOptions: {
-		'ecmaVersion': 2019,
-		'sourceType': 'module',
-		'ecmaFeatures': {
-			jsx: true
-		}
-	},
-	rules: {
-		'@typescript-eslint/no-var-requires': 0,
-		'@typescript-eslint/member-delimiter-style': [
-			2,
-			{
-				multiline: {
-					delimiter: 'semi',
-					requireLast: true,
-				},
-				singleline: {
-					delimiter: 'semi',
-					requireLast: true,
-				},
-			},
-		],
-		'@typescript-eslint/no-empty-interface': [
-			'error',
-			{
-				'allowSingleExtends': true
-			}
-		],
-		'quotes': ['error', 'single']
+		project: './tsconfig.json'
 	}
 };
