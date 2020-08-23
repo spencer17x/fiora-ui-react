9dd2536196f3782a3b3fe7780d4e5d892ca4af3f
Spencer17x <1253478653@qq.com>
2020-08-23 22:21:09 +0800

    refactor: 💡 删除sass-resources-loader

--- webpack.common.js
+++ webpack.common.js
@@ -23,13 +23,7 @@ module.exports = {
 				use: [
 					'style-loader', // 将 JS 字符串生成为 style 节点
 					'css-loader', // 将 CSS 转化成 CommonJS 模块
-					'sass-loader', // 将 Sass 编译成 CSS，默认使用 Node Sass
-					{
-						loader: 'sass-resources-loader',
-						options: {
-							resources: './lib/style/index.scss'
-						}
-					}
+					'sass-loader' // 将 Sass 编译成 CSS，默认使用 Node Sass
 				]
 			},
 			{
--- webpack.components.js
+++ webpack.components.js
@@ -25,12 +25,6 @@ module.exports = merge(prod, {
           },
           'css-loader', // 将 CSS 转化成 CommonJS 模块
           'sass-loader', // 将 Sass 编译成 CSS，默认使用 Node Sass
-					{
-						loader: 'sass-resources-loader',
-						options: {
-							resources: './lib/style/index.scss'
-						}
-					}
         ]
       }
     ]
