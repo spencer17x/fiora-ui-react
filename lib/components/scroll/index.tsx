9d689e9b6e9d09f795522cf2c5ab19b1186107a6
Spencer17x <1253478653@qq.com>
2020-08-26 21:33:10 +0800

    feat: 🎸 完成scrollBar的拖动

--- lib/components/scroll/index.tsx
+++ lib/components/scroll/index.tsx
@@ -1,4 +1,4 @@
-import React, { UIEventHandler, useEffect, useRef, useState } from 'react';
+import React, { MouseEventHandler, UIEventHandler, useEffect, useRef, useState } from 'react';
 import { getScrollbarWidth } from '../../utils';
 import './index.scss';
 
@@ -10,6 +10,8 @@ const Scroll: React.FC<ScrollProps> = props => {
   const scrollWidth = `${getScrollbarWidth() || 20}px`;
   const [barHeight, setBarHeight] = useState(0);
   const [barScrollTop, setBarScrollTop] = useState(0);
+  const move = useRef(false);
+  const startY = useRef(0);
   const containerRef = useRef<HTMLDivElement>(null);
   const onScroll: UIEventHandler = event => {
     const dom = containerRef.current;
@@ -21,6 +23,28 @@ const Scroll: React.FC<ScrollProps> = props => {
       setBarScrollTop(barScrollTop);
     }
   }
+  const onMouseDown: MouseEventHandler = event => {
+    const { clientY } = event;
+    move.current = true;
+    startY.current = clientY;
+  }
+  const onMouseMove = (event: MouseEvent) => {
+    const { clientY } = event;
+    if (move.current) {
+      setBarScrollTop(clientY - startY.current);
+    }
+  }
+  const onMouseUp = () => {
+    move.current = false;
+  }
+  useEffect(() => {
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+    return () => {
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    }
+  }, []);
   useEffect(() => {
     const dom = containerRef.current;
     if (dom) {
@@ -39,8 +63,9 @@ const Scroll: React.FC<ScrollProps> = props => {
     >
       {children}
     </div>
-    <div className='f-scroll--container' style={{ width: scrollWidth }}>
+    <div className='f-scroll--container'>
       <div
+        onMouseDown={onMouseDown}
         className='f-scroll--bar'
         style={{height: barHeight, transform: `translateY(${barScrollTop}px)`}}
       ></div>
